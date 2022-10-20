6335d031f1cc1bca1a97346730784cf7a7e0e08a
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-10-20 08:24:34 +0100

    Remove c++ code for generatePassword()

--- src/main.rs
+++ src/main.rs
@@ -31,34 +31,6 @@ fn main() {
 }
 
 /*
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cstring>
-#include <cmath>
-
-std::string generatePassword(int length)
-{
-    char characters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '\'', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', '"', '@', '#', '~', ',', '<', '.', '>', '/', '?'};
-
-    int counter = 0;
-    std::string password = "";
-
-    while (counter < length)
-    {
-        double percentageProgress = ((double)counter / (double)length * 100.0) + 1.0;
-        if (std::fmod(percentageProgress, 1) == 0)
-        {
-            std::cout << percentageProgress << " complete." << std::endl;
-        }
-        // password.append(characters[Math.round(random.nextInt(characters.length))]); // in case it doesn't work
-        password += characters[rand() % std::strlen(characters)];
-        counter++;
-    }
-
-    return password;
-}
-
 int main(int argc, char const *argv[])
 {
     std::cout << "Enter number of characters: ";
