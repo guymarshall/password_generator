1dcf799d2ff592d87fb08730174fdbb34d593324
Guy Marshall <guymarshall9@gmail.com>
2022-12-16 23:42:15 +0000

    Add commented code that needs finishing to allow program to periodically append string to file - currently overrides file

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,19 @@ use rayon::prelude::*;
 fn generate_password(length: i64) -> String {
     let characters: [char; 91] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '\'', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', '"', '@', '#', '~', ',', '<', '.', '>', '/', '?'];
 
+    // let mut counter: i64 = length;
+
+    // while counter > 0 {
+    //     let mut start: i64 = counter - 1_000_000;
+    //     if start < 0 {
+    //         start = 0;
+    //     }
+    //     let random_chars: Vec<char> = (start..counter).into_par_iter().map(|_| characters[rand::thread_rng().gen_range(0..characters.len() - 1)]).collect();
+    //     let password: String = random_chars.into_par_iter().collect();
+    //     fs::write("password.txt", password).expect("Unable to write file.");
+
+    //     counter -= 1_000_000;
+    // }
     let random_chars: Vec<char> = (0..length).into_par_iter().map(|_| characters[rand::thread_rng().gen_range(0..characters.len() - 1)]).collect();
     random_chars.into_par_iter().collect()
 }
@@ -17,5 +30,6 @@ fn main() {
     let password: String = generate_password(number_of_characters);
 
     println!("Saving password to file...");
+    // generate_password(number_of_characters);
     fs::write("password.txt", password).expect("Unable to write file.");
 }
