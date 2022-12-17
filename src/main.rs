5b28c38795f288187cad561253be8374a63c0870
Guy Marshall <guymarshall9@gmail.com>
2022-12-17 00:06:33 +0000

    Add characters_length variable so it doesn't have to be calculated with every pass

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use rayon::prelude::*;
 
 fn generate_password(length: i64) -> String {
     let characters: [char; 91] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '\'', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', '"', '@', '#', '~', ',', '<', '.', '>', '/', '?'];
+    let characters_length: usize = characters.len();
 
     // let mut counter: i64 = length;
 
@@ -20,7 +21,7 @@ fn generate_password(length: i64) -> String {
 
     //     counter -= 1_000_000;
     // }
-    let random_chars: Vec<char> = (0..length).into_par_iter().map(|_| characters[rand::thread_rng().gen_range(0..characters.len() - 1)]).collect();
+    let random_chars: Vec<char> = (0..length).into_par_iter().map(|_| characters[rand::thread_rng().gen_range(0..characters_length - 1)]).collect();
     random_chars.into_par_iter().collect()
 }
 
