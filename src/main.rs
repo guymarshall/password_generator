49593f17703d02cc9fba8f85aa7301a7088d92e7
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-01-12 12:16:33 +0000

    Remove old commented code

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,6 @@ fn generate_password(length: i64) -> String {
     let characters: [char; 91] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '\'', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', '"', '@', '#', '~', ',', '<', '.', '>', '/', '?'];
     let characters_length: usize = characters.len();
 
-    // let mut counter: i64 = length;
-
-    // while counter > 0 {
-    //     let mut start: i64 = counter - 1_000_000;
-    //     if start < 0 {
-    //         start = 0;
-    //     }
-    //     let random_chars: Vec<char> = (start..counter).into_par_iter().map(|_| characters[rand::thread_rng().gen_range(0..characters.len() - 1)]).collect();
-    //     let password: String = random_chars.into_par_iter().collect();
-    //     fs::write("password.txt", password).expect("Unable to write file.");
-
-    //     counter -= 1_000_000;
-    // }
     let random_chars: Vec<char> = (0..length).into_par_iter().map(|_| characters[rand::thread_rng().gen_range(0..characters_length - 1)]).collect();
     random_chars.into_par_iter().collect()
 }
